802c9c5a80ecd85eb22de65b29c824b024c24443
Kevin Noonan <KJNoonan52@gmail.com>
2015-04-30 14:36:54 -0500

    Adds UI elements and an indication key

--- CSC360/game/game.js
+++ CSC360/game/game.js
@@ -7,30 +7,18 @@
 // Warning! The function tryDig and the variable board are global variables!
 
 $(function () {
-  
-  /*tryDig = function(targetCell)
-  {
-    var targetObj = board.dig(targetCell);
-  
-    if (targetObj) {
-      alert('Success finding the ' + targetObj.name);
-      $("#cell"+targetCell).html('=');
-      $("#cell"+targetCell).css('color', 'red');
-    }
-    else {
-      alert('Failure!');
-      $("#cell"+targetCell).html('O').css('color', 'green');
-    }
-  }*/
-    
     
   board = new GameBoard();
   board.setBoard();
  
   var siteList = $('#siteList');
   $.each(board.ruins, function(index, value){
-    siteList.append(value.name+Array(value.size+1).join("&#8962;")+'<br>');
-
+    siteList.append(value.name+"&nbsp;");
+    //+Array(value.size+1).join("<span id="+value.name+">&#8962;</span>")+'<br>');
+    for(i=0; i<value.size; i++){
+      siteList.append("<span id="+value.name+i+">&#8962;&nbsp;</span>");  
+    }
+    siteList.append("<br>");
   });
 
   var colSelect = $('#colSelection');
@@ -45,6 +33,20 @@ $(function () {
     rowSelect.append($('<option></option>').val(col).html(col));
   }
 
-  //tryDig("c4");
-
+  $('#digBtn').onclick() = function(){tryDig()};
 });
+
+function tryDig(){
+  var targetCell = $('#rowSelection').val()+ $('#colSelection').val();
+  var targetObj = board.dig(targetCell);
+
+  if(targetObj){
+    alert('Success finding the ' + targetObj.name);
+      $("#cell"+targetCell).html('=');
+      $("#cell"+targetCell).css('color', 'red');
+  }
+  else {
+      alert('Failure!');
+      $("#cell"+targetCell).html('O').css('color', 'green');
+    }
+}
